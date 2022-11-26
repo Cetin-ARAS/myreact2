9bc2e6d89ff68a7377739b07a291c4c2c4ca5750
ÇETİN <cetinnnarasss@gmail.com>
2022-11-27 01:03:58 +0300

    props driling

--- src/components/container.js
+++ src/components/container.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Header from './header'
 
 export default function Container(props){
 
     return(
       <div style={{width:"400px", height:"400px", border:"2px solid black", margin:"0px auto"}}>
-        <p>{props.title}</p>
-        <p>{props.number}</p>
+        <Header baslik={props.title}/>
+       
       </div>
     )
  }

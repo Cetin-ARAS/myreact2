1e6ee6584b986ce5e5c784a4a7e583dd3b8c512b
ÇETİN <cetinnnarasss@gmail.com>
2022-11-26 23:09:51 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React,{useState} from 'react'
-import './App.css';
-
-
-function App() {
-  const [deger,setDeger]=useState(0)
-  //?   function Azalt(){
-    //?    setDeger(deger+1)
-  //?}     böyle olursa onClick={Azalt} yazılır button kısmına
+import "./App.css";
+import Container from "./components/container";
+ function App() {
   
-  const Azalt=()=>(setDeger(deger-2))
+  const [baslik,setBaslık]=useState("React Öğreniyorum")
+  const numara=1923;
   return (
-    <div className="App">
-      <p><button onClick={()=>{setDeger(deger+1)}}>Arttır</button></p>
-      <p>{deger}</p>
-      <p><button onClick={Azalt}>Azalt</button></p>
-     
+    <div className='App'>
+      <Container title={baslik} number={numara}/>
     </div>
-  );
+  )
 }
 
+
+
+
 export default App;
+
---
+++ src/components/container.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Container(props){
+    return(
+      <div style={{width:"400px", height:"400px", border:"2px solid black", margin:"0px auto"}}>
+        <p>{props.title}</p>
+        <p>{props.number}</p>
+      </div>
+    )
+ }
+

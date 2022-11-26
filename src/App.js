92c8bccfa98835093ccd5f64af001bbce0013b99
ÇETİN <cetinnnarasss@gmail.com>
2022-11-27 00:54:16 +0300

    container js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 import Container from "./components/container";
  function App() {
   
-  const [baslik,setBaslık]=useState("React Öğreniyorum")
+  const [baslik,setBaslik]=useState("React Öğreniyorum")
   const numara=1923;
   return (
     <div className='App'>
       <Container title={baslik} number={numara}/>
+      <button onClick={()=>{setBaslik("React öğreniyorum Baslığını Değiştirdim.")}}>Değiştir</button>
     </div>
   )
 }
--- src/components/container.js
+++ src/components/container.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function Container(props){
+
     return(
       <div style={{width:"400px", height:"400px", border:"2px solid black", margin:"0px auto"}}>
         <p>{props.title}</p>

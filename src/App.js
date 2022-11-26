a572402ded5c286c8464ef0ea2620cdef71c323c
ÇETİN <cetinnnarasss@gmail.com>
2022-11-26 20:09:27 +0300

    state

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import './App.css';
 
 function App() {
   const [deger,setDeger]=useState(0)
+  //?   function Azalt(){
+    //?    setDeger(deger+1)
+  //?}     böyle olursa onClick={Azalt} yazılır button kısmına
+  
+  const Azalt=()=>(setDeger(deger-2))
   return (
     <div className="App">
       <p><button onClick={()=>{setDeger(deger+1)}}>Arttır</button></p>
       <p>{deger}</p>
-      <p><button onClick={()=>{setDeger(deger-1)}}>Azalt</button></p>
+      <p><button onClick={Azalt}>Azalt</button></p>
      
     </div>
   );

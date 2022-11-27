22b6742eff7f0fcdd281286d62e8509fd8a4895f
ÇETİN <cetinnnarasss@gmail.com>
2022-11-28 00:10:40 +0300

    useContext

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./App.css";
 import Icerik from './components/icerik'
 
-
+const tema={
+  dark:{
+    color:"white",
+    backgroundColor:"black"
+  },
+  light:{
+    color:"black",
+    backgroundColor:"white"
+  }
+};
 export const TemaVerisi=React.createContext();
  function App() {
-
+  const [mod,setMod]=useState(tema.dark);
   const isim="Cetin Aras"
+
+  function değiştir(){
+    if(tema.dark===mod){
+      setMod(tema.light)
+    }
+    else{
+      setMod(tema.dark)
+    }
+  }
   return (
     <div className='App'>
+      <button onClick={değiştir}>
+        {tema.dark===mod ? "Açık":"Koyu"}
+      </button>
       <TemaVerisi.Provider value={isim}>
       <Icerik/>
       </TemaVerisi.Provider>

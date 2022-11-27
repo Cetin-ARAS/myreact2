bcb88b74295e04a8c602d709a1e3b349cbfd751f
ÇETİN <cetinnnarasss@gmail.com>
2022-11-28 00:43:37 +0300

    .

--- src/components/icerik.js
+++ src/components/icerik.js
@@ -6,7 +6,7 @@ export default function Icerik() {
   return (
     <div style={useContext(TemaVerisi)}>
         <h1>React Öğreniyorum</h1>
-        <h2>useContect ile componentler arası veri taşıma</h2>
+        <h2>useContext ile componentler arası veri taşıma</h2>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero voluptate quisquam optio excepturi rerum modi ducimus eligendi ratione? Facere atque optio ratione recusandae quibusdam voluptate ipsa esse pariatur enim et.</p>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero voluptate quisquam optio excepturi rerum modi ducimus eligendi ratione? Facere atque optio ratione recusandae quibusdam voluptate ipsa esse pariatur enim et.</p>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero voluptate quisquam optio excepturi rerum modi ducimus eligendi ratione? Facere atque optio ratione recusandae quibusdam voluptate ipsa esse pariatur enim et.</p>

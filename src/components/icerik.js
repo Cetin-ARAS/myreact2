646d477d386db7a384ec5516ad2bfad05533fdbe
ÇETİN <cetinnnarasss@gmail.com>
2022-11-28 00:43:09 +0300

    finish

--- src/components/icerik.js
+++ src/components/icerik.js
@@ -4,8 +4,7 @@ import {TemaVerisi} from '../App'
 
 export default function Icerik() {
   return (
-    <div>
-        <p>{useContext(TemaVerisi)}</p>
+    <div style={useContext(TemaVerisi)}>
         <h1>React Öğreniyorum</h1>
         <h2>useContect ile componentler arası veri taşıma</h2>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero voluptate quisquam optio excepturi rerum modi ducimus eligendi ratione? Facere atque optio ratione recusandae quibusdam voluptate ipsa esse pariatur enim et.</p>

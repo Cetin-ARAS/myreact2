c7d6d7f636a0c354b3d9aadd9041fbe67f8d1290
ÇETİN <cetinnnarasss@gmail.com>
2022-11-28 14:00:06 +0300

    cont.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 function App () {
   
   const [count,setCount]=useState(0);
+  const [title,setTitle]=useState("Effect");
 
   useEffect(()=>{
   console.log(count)
-  })
+  },[title]);
 
 
 
   return (
     <div className='App'>
       <button onClick={()=>{setCount(count+1)}}>Arttır</button>
+      <p>{title}</p>
 
     </div>
   );
